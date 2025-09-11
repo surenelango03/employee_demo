d8112f574bdfa279cfb10c510d98bf4c8e073583
surenelango03 <suren.elango2930@gmail.com>
2025-09-11 11:49:52 +0530

    postbranch1

--- src/main/java/com/example/employee_demo/dao/EmployeeDetailsDao.java
+++ src/main/java/com/example/employee_demo/dao/EmployeeDetailsDao.java
@@ -1,7 +1,9 @@
 package com.example.employee_demo.dao;
 
 import com.example.employee_demo.dto.EmployeeDTO;
+import com.example.employee_demo.service.EmployeeRequest;
 
 public interface EmployeeDetailsDao {
     EmployeeDTO getEmployeeNameAndSalary(Long id);
+    void addEmployee(EmployeeRequest employee);
 }
--- src/main/java/com/example/employee_demo/dao/EmployeeDetailsDaoImpl.java
+++ src/main/java/com/example/employee_demo/dao/EmployeeDetailsDaoImpl.java
@@ -1,6 +1,7 @@
 package com.example.employee_demo.dao;
 
 import com.example.employee_demo.dto.EmployeeDTO;
+import com.example.employee_demo.service.EmployeeRequest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,7 @@ public class EmployeeDetailsDaoImpl implements EmployeeDetailsDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    
+    @Override
     public EmployeeDTO getEmployeeNameAndSalary(Long id) {
         String sql = "SELECT name, salary FROM employee WHERE id = ?";
         return jdbcTemplate.queryForObject(
@@ -20,5 +21,11 @@ public class EmployeeDetailsDaoImpl implements EmployeeDetailsDao {
             (rs, _) -> new EmployeeDTO(rs.getString("name"), rs.getDouble("salary")),
             id
         );
+    }
+
+    @Override
+    public void addEmployee(EmployeeRequest employee) {
+        String sql = "INSERT INTO employee (name, salary) VALUES (?, ?)";
+        jdbcTemplate.update(sql, employee.getName(), employee.getSalary());
     }
 }
